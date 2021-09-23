31ab68e2d47a3d1bdaaa19288673e4c72fe8c37e
ALMA_DEV914 <aungonalna58@gamil.com>
2021-09-23 10:48:58 -0700

    Add conditions and decisions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-/* var playerName = window.prompt("What is your robot's name?");
+/*var playerName = window.prompt("What is your robot's name?");
 console.log(playerName);
 console.log("This logs a string, good for leaving yourself a message");
 console.log(10 + 10);
@@ -6,8 +6,9 @@ console.log("Our robot's name is " + playerName);
 function fight(){
     window .alert ("The fight has begun!");
 }
-//fight();*/
+//fight(); */
 var playerName = window.alert("What is your robot's name?");
+var playerName = window.prompt("Your robot name");
 var playerHealth = 100;
 var playerAttack = 10;
 console.log(playerName, playerAttack, playerHealth);
@@ -23,11 +24,23 @@ var fight = function(){
 enemyHealth = enemyHealth - playerAttack;
 // Log a resulting message to the console so we know that it worked
 console.log(playerName + " attacked " + enemyName + "." + enemyName + " now has " +  enemyHealth + " health remaining. "); 
+//checked enemy's health
+if (enemyHealth <= 0){
+    window.alert(enemyName + " had died!");
+}
+else{
+    window .alert(enemyName + " still has " + enemyHealth + " health left. ");
+}
 
 //Substract the  value of enemyAttack from the value of playerHealth
 playerHealth = playerHealth - playerAttack;
 // log a resulting message to the console so we know it worked
 console.log(enemyName + " attacked " + playerName + "." +  playerName + " now has " + playerHealth + " health remaining");
+// checked playerhealth
+if (playerHealth <= 0){
+    window.alert(playerName + " has died");
+}
+else(playerName + " has still " + playerHealth + " health left.");
 }
 fight(); 
 
